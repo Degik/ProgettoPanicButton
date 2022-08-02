0bb7572a63629a35c2c38244bef44c242236d621
Degik <prodax9900@gmail.com>
2022-08-03 01:05:58 +0200

    Updated

--- app/src/main/java/com/example/progettopanicbutton/MainActivity.java
+++ app/src/main/java/com/example/progettopanicbutton/MainActivity.java
@@ -2,10 +2,17 @@ package com.example.progettopanicbutton;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,8 +21,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     private BottomNavigationView bottomNavigationView;
     //
     //Fragment
-    Contacts contactsFragment = new Contacts();
-    Panic panicFragment = new Panic();
+    private Contacts contactsFragment = new Contacts();
+    private Panic panicFragment = new Panic();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,14 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
         bottomNavigationView.setSelectedItemId(R.id.navigation_panic);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+
+        /*
+            Raccolgo un valore dalle sharedPref per prova
+        */
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String signature = sharedPreferences.getString("signature", "");
+        System.out.println(signature);
     }
 
     @Override
@@ -40,5 +55,28 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 return true;
         }
         return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.menu_settings:
+                newSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void newSettings(){
+        Intent intentSettings = new Intent(this, SettingsActivity.class);
+        startActivity(intentSettings);
     }
 }
